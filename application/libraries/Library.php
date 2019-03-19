5de2aca54c3f01ff5bbfceaa170707d082074ff5
Jijin Sics <jijin.sics@gmail.com>
2019-03-19 17:28:12 +0530

    lib file modified

--- application/libraries/Library.php
+++ application/libraries/Library.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace app\libraries;
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
@@ -14,39 +14,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
  */
 //this class loaded in index
-class Library///core library fil
+class Library   ///core library fil
 
 {
     public function __construct()
     {
         $this->ci = &get_instance();
         ///Models
-        $this->ci->load->model('api/crud', 'crud');
-        $this->ci->load->model('api/v1/teams_model', 'teams_model');
-        $this->ci->load->model('api/v1/user_model', 'user_model');
-        $this->ci->load->model('api/v1/posts_model', 'posts_model');
-        $this->ci->load->model('api/v1/wall_model', 'wall_model');
-        $this->ci->load->model('api/v1/Athlete_model', 'athlete_model');
-        $this->ci->load->model('api/v1/Activity_model', 'activity_model');
-        $this->ci->load->model('api/v1/Dare_model', 'dare_model');
-
-        //tables
-        $this->table['users'] = $this->ci->config->item('users', 'table');
-        $this->table['roles'] = $this->ci->config->item('roles', 'table');
-        $this->table['gambits'] = $this->ci->config->item('gambits', 'table');
-        $this->table['fans'] = $this->ci->config->item('fans', 'table');
-        $this->table['users_social'] = $this->ci->config->item('users_social', 'table');
-        $this->table['teams'] = $this->ci->config->item('teams', 'table');
-        $this->table['team_members'] = $this->ci->config->item('team_members', 'table');
-        $this->table['claps'] = $this->ci->config->item('claps', 'table');
-        $this->table['comments'] = $this->ci->config->item('comments', 'table');
-        $this->table['posts'] = $this->ci->config->item('posts', 'table');
-        $this->table['posts_media'] = $this->ci->config->item('posts_media', 'table');
-        $this->table['account_types'] = $this->ci->config->item('account_types', 'table');
-        $this->table['athlete_sports_category'] = $this->ci->config->item('athlete_sports_category', 'table');
-        $this->table['business_details'] = $this->ci->config->item('business_details', 'table');
-        $this->table['business_types'] = $this->ci->config->item('business_types', 'table');
-        $this->table['user_activity'] = $this->ci->config->item('user_activity', 'table'); //main table
+      
 
         //Variables
         // $this->default_level = $this->ci->config->item('default_level');
@@ -83,11 +58,14 @@ class Library///core library fil
                 if ($this->ci->upload->do_upload('file')) {
 
                     $imageData = $this->ci->upload->data();
-                    $output = array("name" => $imageData['file_name'], "type" => $imageData['file_type'], "status" => true);
+                    $output = array("name" => $imageData['file_name'], "type" => $imageData['file_type'], "status" => true, "error_data" => $this->ci->upload->display_errors());
+
+                    
 
                 } else {
 
                     $output['status'] = false;
+                    $output['error_data'] = $this->ci->upload->display_errors();
 
                 }
             }
@@ -97,7 +75,15 @@ class Library///core library fil
 
             if ($this->ci->upload->do_upload($field_key)) {
                 $imageData = $this->ci->upload->data();
-                $output = array("name" => $imageData['file_name'], "type" => $imageData['file_type'], "status" => true);
+                $output = array("name" => $imageData['file_name'], "type" => $imageData['file_type'], "status" => true,"error_data"=> $this->ci->upload->display_errors());
+            } else {
+
+                $output['status'] = false;
+                $output['name'] ="default.png";
+                $output[ 'type'] = "image/jpeg";
+
+                $output['error_data'] = $this->ci->upload->display_errors();
+
             }
         }
 
