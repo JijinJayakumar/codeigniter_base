90801642e150e52a1a7abd150b6d9354e3bf2294
Jijin <jijinjayakumaronline@gmail.com>
2019-07-14 11:39:34 +0530

    Added crud helper

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,15 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 // use models\Welcome_model;
 // use app\helpers\ArrayHelper;
 use app\models\Welcome_model;
-use app\libraries\REST_Controller;
 use Cartalyst\Sentinel\Native\Facades\Sentinel;
-// use Restserver\Libraries\Format;
-class Welcome extends  \CI_Controller {
 
-	
-    function __construct()
+// use Restserver\Libraries\Format;
+class Welcome extends \CI_Controller
+{
+
+    public function __construct()
     {
         // Construct the parent class
         parent::__construct();
@@ -19,64 +19,72 @@ class Welcome extends  \CI_Controller {
         // $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         // $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
 	}
+
+
+	public function index(){
+
+		echo "		Welcome to CI 		";
+
+	}
 	
-	public function users_get(){
-		echo "working";
-	}
-
-	public function index_get()
-	{
-		$this->load->model('TestModel');
-
-		// print_r($this->TestModel->test());
-		 $users = Welcome_model::all();
-		 $this->response($users);
-
-	}
 
 
-	public function index_post(){
-		//echo "test working";
-	}
+    public function users_get()
+    {
+        echo "working";
+    }
 
-	public function sentinel(){
+    public function index_get()
+    {
+        $this->load->model('TestModel');
 
-		$credentials = [
-			'email'    => 'admin@admin.com',
-			'password' => '123456',
-		];
-		
-		//  $user=Sentinel::registerAndActivate($credentials);
-// 		  $user=Sentinel::authenticate($credentials);
-// echo $user->id;
-		// print_r($user);
-		// $role = Sentinel::getRoleRepository()->createModel()->create([
-		// 	'name' => 'SuperAdmin',
-		// 	'slug' => 'superadmin',
-		// ]);
+        // print_r($this->TestModel->test());
+        $users = Welcome_model::all();
+        $this->response($users);
 
-				// 		$role = Sentinel::findRoleById(1);
+    }
 
-				// $role->permissions = [
-				// 	'user.update' => true,
-				// 	'user.view' => true,
-				// ];
+    public function index_post()
+    {
+        //echo "test working";
+    }
 
-				// $role->save();
-		
+    public function sentinel()
+    {
 
-	// $user = Sentinel::findById(1);
-	// $role = Sentinel::findRoleByName('SuperAdmin');
-	// $role->users()->attach($user);
+        $credentials = [
+            'email' => 'admin@admin.com',
+            'password' => '123456',
+        ];
 
+        //  $user=Sentinel::registerAndActivate($credentials);
+        //           $user=Sentinel::authenticate($credentials);
+        // echo $user->id;
+        // print_r($user);
+        // $role = Sentinel::getRoleRepository()->createModel()->create([
+        //     'name' => 'SuperAdmin',
+        //     'slug' => 'superadmin',
+        // ]);
 
-// 	$user = Sentinel::findById(1);
+        //         $role = Sentinel::findRoleById(1);
+
+        // $role->permissions = [
+        //     'user.update' => true,
+        //     'user.view' => true,
+        // ];
+
+        // $role->save();
+
+        // $user = Sentinel::findById(1);
+        // $role = Sentinel::findRoleByName('SuperAdmin');
+        // $role->users()->attach($user);
+
+//     $user = Sentinel::findById(1);
 
 // $data=Sentinel::login($user);
-// $admin = Sentinel::inRole('superadmin');
-// echo json_encode($admin);
-// echo getenv('DB_HOSTNAME');
-	}
+        // $admin = Sentinel::inRole('superadmin');
+        // echo json_encode($admin);
+        // echo getenv('DB_HOSTNAME');
+    }
 
-	
 }
